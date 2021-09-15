bc4b1e921083fa9304bb3dd3ab872a27b94ef8b6
Felipe Ximenes <f.ximenes@portaltelemedicina.com.br>
2021-09-15 12:13:36 -0300

    Change kubernetes dag file

--- dags/kubernetespodopertor.py
+++ dags/kubernetespodopertor.py
@@ -1,162 +1,84 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-"""
-This is an example dag for using the KubernetesPodOperator.
-"""
+import datetime as dt
+import os
 
 from kubernetes.client import models as k8s
 
 from airflow import DAG
-from airflow.kubernetes.secret import Secret
-from airflow.operators.bash import BashOperator
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
-from airflow.utils.dates import days_ago
-
-# [START howto_operator_k8s_cluster_resources]
-secret_file = Secret('volume', '/etc/sql_conn', 'airflow-secrets', 'sql_alchemy_conn')
-secret_env = Secret('env', 'SQL_CONN', 'airflow-secrets', 'sql_alchemy_conn')
-secret_all_keys = Secret('env', None, 'airflow-secrets-2')
-volume_mount = k8s.V1VolumeMount(
-    name='test-volume', mount_path='/root/mount_file', sub_path=None, read_only=True
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
 )
-
-configmaps = [
-    k8s.V1EnvFromSource(config_map_ref=k8s.V1ConfigMapEnvSource(name='test-configmap-1')),
-    k8s.V1EnvFromSource(config_map_ref=k8s.V1ConfigMapEnvSource(name='test-configmap-2')),
-]
-
-volume = k8s.V1Volume(
-    name='test-volume',
-    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name='test-volume'),
-)
-
-port = k8s.V1ContainerPort(name='http', container_port=80)
-
-init_container_volume_mounts = [
-    k8s.V1VolumeMount(mount_path='/etc/foo', name='test-volume', sub_path=None, read_only=True)
-]
-
-init_environments = [k8s.V1EnvVar(name='key1', value='value1'), k8s.V1EnvVar(name='key2', value='value2')]
-
-init_container = k8s.V1Container(
-    name="init-container",
-    image="ubuntu:16.04",
-    env=init_environments,
-    volume_mounts=init_container_volume_mounts,
-    command=["bash", "-cx"],
-    args=["echo 10"],
-)
-
-affinity = k8s.V1Affinity(
-    node_affinity=k8s.V1NodeAffinity(
-        preferred_during_scheduling_ignored_during_execution=[
-            k8s.V1PreferredSchedulingTerm(
-                weight=1,
-                preference=k8s.V1NodeSelectorTerm(
-                    match_expressions=[
-                        k8s.V1NodeSelectorRequirement(key="disktype", operator="in", values=["ssd"])
-                    ]
-                ),
-            )
-        ]
-    ),
-    pod_affinity=k8s.V1PodAffinity(
-        required_during_scheduling_ignored_during_execution=[
-            k8s.V1WeightedPodAffinityTerm(
-                weight=1,
-                pod_affinity_term=k8s.V1PodAffinityTerm(
-                    label_selector=k8s.V1LabelSelector(
-                        match_expressions=[
-                            k8s.V1LabelSelectorRequirement(key="security", operator="In", values="S1")
-                        ]
-                    ),
-                    topology_key="failure-domain.beta.kubernetes.io/zone",
-                ),
-            )
-        ]
-    ),
-)
-
-tolerations = [k8s.V1Toleration(key="key", operator="Equal", value="value")]
-
-# [END howto_operator_k8s_cluster_resources]
-
 
 with DAG(
-    dag_id='example_kubernetes_operator',
-    schedule_interval=None,
-    start_date=days_ago(2),
-    tags=['example'],
+    dag_id="02_kubernetes",
+    description="Fetches ratings from the Movielens API using kubernetes.",
+    start_date=dt.datetime(2019, 1, 1),
+    end_date=dt.datetime(2019, 1, 3),
+    schedule_interval="@daily",
+    default_args={"depends_on_past": True},
 ) as dag:
-    k = KubernetesPodOperator(
-        namespace='default',
-        image="ubuntu:16.04",
-        cmds=["bash", "-cx"],
-        arguments=["echo", "10"],
-        labels={"foo": "bar"},
-        secrets=[secret_file, secret_env, secret_all_keys],
-        ports=[port],
+
+    volume_claim = k8s.V1PersistentVolumeClaimVolumeSource(claim_name="data-volume")
+    volume = k8s.V1Volume(name="data-volume", persistent_volume_claim=volume_claim)
+
+    volume_mount = k8s.V1VolumeMount(
+        name="data-volume", mount_path="/data", sub_path=None, read_only=False
+    )
+
+    fetch_ratings = KubernetesPodOperator(
+        task_id="fetch_ratings",
+        # Airflow 2.0.0a2 has a bug that results in the pod operator not applying
+        # the image pull policy. By default, the k8s SDK uses a policy of always
+        # pulling the image when using the latest tag, but only pulling an image if
+        # it's not present (what we want) when using a different tag. For now, we
+        # use this behaviour to get our desired image policy behaviour.
+        #
+        # TODO: Remove this workaround when the bug is fixed.
+        #       See https://github.com/apache/airflow/issues/11998.
+        #
+        image="manning-airflow/movielens-fetch:k8s",
+        cmds=["fetch-ratings"],
+        arguments=[
+            "--start_date",
+            "{{ds}}",
+            "--end_date",
+            "{{next_ds}}",
+            "--output_path",
+            "/data/ratings/{{ds}}.json",
+            "--user",
+            os.environ["MOVIELENS_USER"],
+            "--password",
+            os.environ["MOVIELENS_PASSWORD"],
+            "--host",
+            os.environ["MOVIELENS_HOST"],
+        ],
+        namespace="airflow",
+        name="fetch-ratings",
+        cluster_context="docker-desktop",
+        in_cluster=False,
         volumes=[volume],
         volume_mounts=[volume_mount],
-        env_from=configmaps,
-        name="airflow-test-pod",
-        task_id="task",
-        affinity=affinity,
+        image_pull_policy="Never",
         is_delete_operator_pod=True,
-        hostnetwork=False,
-        tolerations=tolerations,
-        init_containers=[init_container],
-        priority_class_name="medium",
     )
 
-    # [START howto_operator_k8s_private_image]
-    quay_k8s = KubernetesPodOperator(
-        namespace='default',
-        image='quay.io/apache/bash',
-        image_pull_secrets=[k8s.V1LocalObjectReference('testquay')],
-        cmds=["bash", "-cx"],
-        arguments=["echo", "10", "echo pwd"],
-        labels={"foo": "bar"},
-        name="airflow-private-image-pod",
+    rank_movies = KubernetesPodOperator(
+        task_id="rank_movies",
+        image="manning-airflow/movielens-rank:k8s",
+        cmds=["rank-movies"],
+        arguments=[
+            "--input_path",
+            "/data/ratings/{{ds}}.json",
+            "--output_path",
+            "/data/rankings/{{ds}}.csv",
+        ],
+        namespace="airflow",
+        name="rank-movies",
+        cluster_context="docker-desktop",
+        in_cluster=False,
+        volumes=[volume],
+        volume_mounts=[volume_mount],
+        image_pull_policy="Never",
         is_delete_operator_pod=True,
-        in_cluster=True,
-        task_id="task-two",
-        get_logs=True,
-    )
-    # [END howto_operator_k8s_private_image]
-
-    # [START howto_operator_k8s_write_xcom]
-    write_xcom = KubernetesPodOperator(
-        namespace='default',
-        image='alpine',
-        cmds=["sh", "-c", "mkdir -p /airflow/xcom/;echo '[1,2,3,4]' > /airflow/xcom/return.json"],
-        name="write-xcom",
-        do_xcom_push=True,
-        is_delete_operator_pod=True,
-        in_cluster=True,
-        task_id="write-xcom",
-        get_logs=True,
     )
 
-    pod_task_xcom_result = BashOperator(
-        bash_command="echo \"{{ task_instance.xcom_pull('write-xcom')[0] }}\"",
-        task_id="pod_task_xcom_result",
-    )
-    # [END howto_operator_k8s_write_xcom]
-
-    write_xcom >> pod_task_xcom_result
+    fetch_ratings >> rank_movies
