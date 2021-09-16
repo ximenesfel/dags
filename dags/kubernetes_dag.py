024c0f6ccc0b9ede332217d7016bf1408a0fa8dc
Felipe Ximenes <f.ximenes@portaltelemedicina.com.br>
2021-09-16 17:06:49 -0300

    Change KubernetesPodOperator namespace

--- dags/kubernetes_dag.py
+++ dags/kubernetes_dag.py
@@ -26,25 +26,25 @@ dag = DAG(
 )
 
 # Generate 2 tasks
-tasks = ["task{}".format(i) for i in range(1, 3)]
-example_dag_complete_node = DummyOperator(task_id="example_dag_complete", dag=dag)
+# tasks = ["task{}".format(i) for i in range(1, 2)]
+# example_dag_complete_node = DummyOperator(task_id="example_dag_complete", dag=dag)
 
-org_dags = []
-for task in tasks:
+# org_dags = []
+# for task in tasks:
 
-    bash_command = 'echo HELLO'
+#     bash_command = 'echo HELLO'
 
-    org_node = KubernetesPodOperator(
-        namespace='airflow',
-        image="tensorflow/tensorflow:2.4.2",
-        cmds=["tensorboard", "--help"],
-        image_pull_policy="Always",
-        name=task,
-        in_cluster=True,
-        task_id=task,
-        is_delete_operator_pod=True,
-        get_logs=True,
-        dag=dag
-    )
+org_node = KubernetesPodOperator(
+    namespace='airflow',
+    image="tensorflow/tensorflow:2.4.2",
+    cmds=["tensorboard", "--help"],
+    image_pull_policy="Always",
+    name=task,
+    in_cluster=True,
+    task_id=task,
+    is_delete_operator_pod=True,
+    get_logs=True,
+    dag=dag
+)
 
-    org_node.set_downstream(example_dag_complete_node)
+org_node
