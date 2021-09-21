2d1d7059acbb99e75196f969247237de8aa4c966
Felipe Ximenes <f.ximenes@portaltelemedicina.com.br>
2021-09-21 16:49:00 -0300

    Include tensorbard service

--- dags/kubernetes_dag.py
+++ dags/kubernetes_dag.py
@@ -55,7 +55,7 @@ probe = k8s.V1Probe(_exec=exec_action)
 run_id = '{{run_id}}'
 
 training = k8s.V1Container(image="ximenesfel/mnist_training:latest", 
-                           command=["python", "/root/code/fashion_mnist.py", "-f", f"run_id"], 
+                           command=["python", "/root/code/fashion_mnist.py", "-f", f"{run_id}"], 
                            name="training",
                            tty=True,
                            volume_mounts=[volume_mount])
