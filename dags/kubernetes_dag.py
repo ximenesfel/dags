14130ec2dc35d2e0acefd4cdea3c01b787c0f87a
Felipe Ximenes <f.ximenes@portaltelemedicina.com.br>
2021-09-22 10:47:52 -0300

    Change kubernetes dag

--- dags/kubernetes_dag.py
+++ dags/kubernetes_dag.py
@@ -52,7 +52,7 @@ training = k8s.V1Container(image="ximenesfel/mnist_training:latest",
 
 tensorboard = k8s.V1Container(image="ximenesfel/mnist_tensorboard:latest", 
                               command=["tensorboard", "--logdir"],
-                              args= ["/root/tensorboard/'{{ run_id }}'", "--bind_all"],
+                              args= ["/root/tensorboard/"'{{ run_id }}', "--bind_all"],
                               name="tensorboard",
                               tty=True,
                               liveness_probe=probe,
