76682dca1034f32793b34c86456da681b3fb5d03
Felipe Ximenes <f.ximenes@portaltelemedicina.com.br>
2021-10-01 17:57:10 -0300

    Include parameter dag

--- dags/aws_s3.py
+++ dags/aws_s3.py
@@ -19,49 +19,29 @@ import os
 from airflow.decorators import task
 from airflow.models.dag import DAG
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-from airflow.providers.amazon.aws.operators.s3_bucket import S3CreateBucketOperator, S3DeleteBucketOperator
 from airflow.utils.dates import days_ago
 
 BUCKET_NAME = os.environ.get('BUCKET_NAME', 'testairflowkuberntes')
 
 
-@task(task_id="s3_bucket_dag_add_keys_to_bucket")
-def upload_keys():
-    """This is a python callback to add keys into the s3 bucket"""
-    # add keys to bucket
-    s3_hook = S3Hook()
-    for i in range(0, 3):
-        s3_hook.load_string(
-            string_data="input",
-            key=f"path/data{i}",
-            bucket_name=BUCKET_NAME,
-        )
-
-
-with DAG(
-    dag_id='s3_bucket_dag',
+dag = DAG(
+    's3_downlaod_file',
+    default_args=default_args,
     schedule_interval=None,
-    start_date=days_ago(2),
-    default_args={"bucket_name": BUCKET_NAME},
     max_active_runs=1,
-    tags=['example'],
-) as dag:
+    concurrency=10,
+    tags=['training'],
+)
 
-    # [START howto_operator_s3_bucket]
-    create_bucket = S3CreateBucketOperator(
-        task_id='s3_bucket_dag_create',
-        aws_conn_id="aws",
-        region_name='us-east-1',
-    )
 
-    # Using a task-decorated function to add keys
-    add_keys_to_bucket = upload_keys()
+def downlaod():
+    s3 = S3Hook(aws_conn_id="aws")
+    s3.download_file(key="./known_hosts",
+                    bucket_name="testairflowkuberntes")
 
-    delete_bucket = S3DeleteBucketOperator(
-        task_id='s3_bucket_dag_delete',
-        aws_conn_id="aws",
-        force_delete=True,
-    )
-    # [END howto_operator_s3_bucket]
+task = PythonOperator(
+    python_callable=download,
+    task_id="download_api_data",
+    dag=dag)
 
-    create_bucket >> add_keys_to_bucket >> delete_bucket
+task
