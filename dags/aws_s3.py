0d2daab0c625bcc3892096028162e960bfb5723d
Felipe Ximenes <f.ximenes@portaltelemedicina.com.br>
2021-10-04 15:59:36 -0300

    Change local path

--- dags/aws_s3.py
+++ dags/aws_s3.py
@@ -1,31 +1,12 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-import os
-import io
-import boto3
-
-from airflow.decorators import task
-from airflow.models.dag import DAG
-from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-from airflow.utils.dates import days_ago
-from airflow.operators.python import PythonOperator
 from datetime import timedelta
-
-BUCKET_NAME = os.environ.get('BUCKET_NAME', 'testairflowkuberntes')
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.models import DagRun
+from airflow.utils.state import State
+from airflow.utils.dates import days_ago
+from kubernetes.client import models as k8s
 
 default_args = {
     'owner': 'Airflow',
@@ -40,7 +21,7 @@ default_args = {
 }
 
 dag = DAG(
-    's3_downlaod_file',
+    'tensorboard',
     default_args=default_args,
     schedule_interval=None,
     max_active_runs=1,
@@ -48,16 +29,41 @@ dag = DAG(
     tags=['training'],
 )
 
+volume_mount = k8s.V1VolumeMount(
+    name='dataset', mount_path='/root/dataset', sub_path=None)
 
-def download():
- 
-    s3hook = S3Hook(aws_conn_id="aws")
-    value = s3hook.download_file(key="known_hosts", bucket_name="testairflowkuberntes", local_path="/tmp/dataset")
-    print(value)
+volume = k8s.V1Volume(
+    name='dataset',
+    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name='dataset-claim'),
+)
 
-task = PythonOperator(
-    python_callable=download,
-    task_id="download_api_data",
-    dag=dag)
+start = BashOperator(
+    task_id='start',
+    bash_command='echo 1',
+    dag=dag
+)
 
-task
+download_dataset = KubernetesPodOperator(
+    namespace='airflow',
+    image="ximenesfel/aws_s3:latest",
+    cmds = ["python", "/root/code/download_file_from_s3_bucket.py", "-p", "s3://testairflowkuberntes", "-i"],
+    arguments=['{{ run_id }}'],
+    name="dataset",
+    in_cluster=True,
+    task_id="dataset",
+    volumes=[volume],
+    volume_mounts=[volume_mount],
+    is_delete_operator_pod=True,
+    startup_timeout_seconds=300,
+    get_logs=True,
+    dag=dag
+)
+
+
+finish = BashOperator(
+    task_id='finish',
+    bash_command='echo 1',
+    dag=dag
+)
+
+start >> download_dataset >> finish
