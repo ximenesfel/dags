27d26aeb0d597c0cc6c2eab67e1dd01dce6f83a6
Felipe Ximenes <f.ximenes@portaltelemedicina.com.br>
2021-10-14 16:25:03 -0300

    Include gcp

--- dags/gcp.py
+++ dags/gcp.py
@@ -1,18 +1,22 @@
 import os
 
 from airflow import models
+from airflow import DAG
+from airflow.utils.dates import days_ago
 from airflow.providers.google.cloud.transfers.gcs_to_local import GCSToLocalFilesystemOperator
 from airflow.providers.google.cloud.operators.gcs import GCSListObjectsOperator
-from airflow.utils.dates import days_ago
-from airflow import DAG
 
 from datetime import timedelta
+
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "tele-covid19")
 BUCKET = os.environ.get("GCP_GCS_BUCKET", "covid-mlengine")
 
 PATH_TO_REMOTE_FILE = os.environ.get("GCP_GCS_PATH_TO_UPLOAD_FILE", "Data/RTXCovidPneumonia/Test/V6")
 PATH_TO_LOCAL_FILE = os.environ.get("GCP_GCS_PATH_TO_SAVED_FILE", "/tmp/data/dataset")
+
+def print_value(ds, **kwargs):
+    print(bucket_files_list)
 
 default_args = {
     'owner': 'Airflow',
@@ -39,6 +43,9 @@ bucket_files_list = GCSListObjectsOperator(
     bucket='covid-mlengine',
     prefix='Data/RTXCovidPneumonia/Test/V6/')
 
-print(bucket_files_list)
+print_data = PythonOperator(
+    task_id='print_the_context',
+    python_callable=print_value,
+)
 
-bucket_files_list
+bucket_files_list >> print_data
