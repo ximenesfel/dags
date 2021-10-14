fc4c457f1668c882852f217b59d3c107466749ba
Felipe Ximenes <f.ximenes@portaltelemedicina.com.br>
2021-10-14 15:44:56 -0300

    Include gcp

---
+++ dags/gcp.py
@@ -0,0 +1,27 @@
+import os
+
+from airflow import models
+from airflow.providers.google.cloud.transfers.gcs_to_local import GCSToLocalFilesystemOperator
+from airflow.utils.dates import days_ago
+
+PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "tele-covid19")
+BUCKET = os.environ.get("GCP_GCS_BUCKET", "covid-mlengine")
+
+PATH_TO_REMOTE_FILE = os.environ.get("GCP_GCS_PATH_TO_UPLOAD_FILE", "Data/RTXCovidPneumonia/Test/V6")
+PATH_TO_LOCAL_FILE = os.environ.get("GCP_GCS_PATH_TO_SAVED_FILE", "/tmp/data/dataset")
+
+
+dag = DAG(
+    'downlaod_file_gcp',
+    default_args=default_args,
+    schedule_interval=None,
+    max_active_runs=1,
+    concurrency=10,
+)
+
+bucket_files_list = GoogleCloudStorageListOperator(
+    task_id='bucket_file_list',
+    bucket='covid-mlengine',
+    prefix='Data/RTXCovidPneumonia/Test/V6/')
+
+print(bucket_files_list)
--- dags/training.py
+++ dags/training.py
@@ -38,18 +38,49 @@ volume = k8s.V1Volume(
     persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name='tensorboard-claim'),
 )
 
+volume_mount_dataset = k8s.V1VolumeMount(
+    name='dataset', mount_path='/root/dataset', sub_path=None)
+
+volume_dataset = k8s.V1Volume(
+    name='dataset',
+    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name='dataset-claim'),
+)
+
+AWS_ACCESS_KEY_ID = Secret(
+   deploy_type="env", deploy_target="AWS_ACCESS_KEY_ID", secret="aws", key="AWS_ACCESS_KEY_ID"
+)
+
+AWS_DEFAULT_REGION = Secret(
+   deploy_type="env", deploy_target="AWS_DEFAULT_REGION", secret="aws", key="AWS_DEFAULT_REGION"
+)
+
+AWS_SECRET_ACCESS_KEY = Secret(
+   deploy_type="env", deploy_target="AWS_SECRET_ACCESS_KEY", secret="aws", key="AWS_SECRET_ACCESS_KEY"
+)
+
 start = BashOperator(
     task_id='start',
     bash_command='echo 1',
     dag=dag
 )
 
-# create_bucket = S3CreateBucketOperator(
-#         task_id='s3_bucket_dag_create',
-#         aws_conn_id="aws",
-#         bucket_name="testairflowkuberntes",
-#         region_name='sa-east-1',
-# )
+download_dataset = KubernetesPodOperator(
+    namespace='airflow',
+    image="ximenesfel/aws_s3:latest",
+    cmds = ["python", "/root/code/download_file_from_s3_bucket.py", "-p", "{{ dag_run.conf['s3_bucket'] }}", "-i"],
+    arguments=['{{ run_id }}'],
+    name="dataset",
+    in_cluster=True,
+    task_id="dataset",
+    volumes=[volume],
+    secrets=[AWS_ACCESS_KEY_ID, AWS_DEFAULT_REGION, AWS_SECRET_ACCESS_KEY],
+    volume_mounts=[volume_mount],
+    is_delete_operator_pod=True,
+    startup_timeout_seconds=300,
+    do_xcom_push=True,
+    get_logs=True,
+    dag=dag
+)
 
 training = KubernetesPodOperator(
     namespace='airflow',
@@ -73,6 +104,4 @@ finish = BashOperator(
     dag=dag
 )
 
-start >> training >> finish
-
-# start >> S3CreateBucketOperator >> finish
+start >> >> download_dataset >> training >> finish
